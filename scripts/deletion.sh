fa317464ee1589750ade351b2f24115d24f37472
chirag1603 <chiragchoudha1603@gmail.com>
2019-12-08 19:43:13 +0000

    restructuring folder

--- scripts/deletion.sh
+++ scripts/deletion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "deletion of application stack"
 docker stack rm javawebapp
+echo "removing the stack completely.......wait for 2 mins for completion...........!!"
 sleep 120
 echo "deletion of application stack successfully"
 docker volume rm javawebapp_data-volume
